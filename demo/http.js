2c30f7f91eae003ad49685a59077355168574c6a
naglec13 <naglec13@gmail.com>
2020-08-02 23:32:19 +0300

    added header in http.js and html index/contact pages

--- demo/http.js
+++ demo/http.js
@@ -1,6 +1,9 @@
 const http = require('http')
 
 const server = http.createServer( (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
     res.end('<h1>Hello NodeJS!!!</h1>')
 })
 server.listen(3000, () => {
