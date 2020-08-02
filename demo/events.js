12e8c2a6e332e16fc61bbce08309e5c5811ed278
naglec13 <naglec13@gmail.com>
2020-08-02 14:59:19 +0300

    added module using internal NodeJS module 'events'

---
+++ demo/events.js
@@ -0,0 +1,36 @@
+const EventEmmiter = require('events')
+
+const emmiter = new EventEmmiter()
+
+emmiter.on('anything',  data => {
+
+    console.log('Listening ON: ', data)
+});
+
+emmiter.emit('anything', {a:221})
+emmiter.emit('anything', {b:-65})
+
+setTimeout(()=>{
+    emmiter.emit('anything', {c:134})
+}, 300)
+
+
+class Dispatcher extends EventEmmiter{
+    subscribe(eventName, cb){
+        console.log('[Subscribe...]')
+        this.on(eventName, cb)
+    }
+
+    dispatch(eventName, data){
+        console.log('[Dispatch...]')
+        this.emit(eventName, data)
+    }
+}
+
+const div = new Dispatcher()
+
+div.subscribe('event', data => {
+    console.log('[event]: ', data)
+});
+
+div.dispatch('event', {h:3})
