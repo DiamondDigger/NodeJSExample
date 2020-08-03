b5cb9d3bcd78b3dfbb64a3070f88d14dc59ebafe
naglec13 <naglec13@gmail.com>
2020-08-03 20:29:52 +0300

    optimize http server, added error.html

--- demo/http.js
+++ demo/http.js
@@ -4,28 +4,59 @@ const path = require('path')
 
 const server = http.createServer((req, res) => {
 
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, '../public', 'index.html'), (err, data) => {
-            if (err) {
-                throw err
-            }
+    // if (req.url === '/') {
+    //     fs.readFile(path.join(__dirname, '../public', 'index.html'), (err, data) => {
+    //         if (err) {
+    //             throw err
+    //         }
+    //
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         })
+    //         res.end(data)
+    //     });
+    // } else if (req.url === '/contact') {
+    //     fs.readFile(path.join(__dirname, '../public', 'contact.html'), (err, data) => {
+    //         if (err) {
+    //             throw err
+    //         }
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         })
+    //         res.end(data)
+    //     })
+    // }
+// })
 
-            res.writeHead(200, {
-                'Content-Type' : 'text/html'
-            })
-            res.end(data)
-        });
-    } else if (req.url==='/contact') {
-        fs.readFile(path.join(__dirname, '../public', 'contact.html'), (err, data) => {
-            if (err) {
-                throw err
-            }
-            res.writeHead(200, {
-                'Content-Type':'text/html'
-            })
-            res.end(data)
-        })
+    let filepath = path.join(__dirname, '../public', req.url === '/' ? 'index.html' : req.url)
+
+    console.log(filepath)
+
+    let ext = path.extname(filepath)
+
+    if (!ext) {
+        filepath += '.html'
     }
+    fs.readFile(filepath, (err, content) => {
+        if (err) {
+            fs.readFile(path.join(__dirname, './public', 'error.html'), (err, data) => {
+                if (err) {
+                    res.writeHead(500)
+                    res.end('Error')
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    })
+                    res.end(data)
+                }
+            })
+        } else{
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.end(content)
+        }
+    });
 })
 
 server.listen(3000, () => {
